2294dd2c6e0ff11163d1204f7763191e5e97b7a2
Femi Lawal <lawalshalom@gmail.com>
2021-01-03 16:35:34 +0100

    fixes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const BinaryFile = require('binary-file');
+const path = require("path");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
