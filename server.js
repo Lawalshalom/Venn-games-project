ed73ee8b2545cb680183b1b236e458e470672aad
Femi Lawal <lawalshalom@gmail.com>
2021-01-03 16:34:33 +0100

    fixes

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static('public'));
 app.use(cors());
-
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
